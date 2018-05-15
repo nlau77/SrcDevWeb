d1aa83184f439fe38f07e10ba6cf06c6a6917a72
nlau77 <nlau777@gmail.com>
2018-05-15 11:52:45 -0400

    second commit

---
+++ WebApplicationTest/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<string> nicholeList = new List<string>{ "Nikkole", "Nikhole", "Ny'kole", "Nycole", "Nicholle", "Nacole", "Nachole", "Nicchole" };
+            string randNichole = nicholeList.OrderBy(x => Guid.NewGuid()).FirstOrDefault();
+            //Random rnd = new Random();
+            //int r = rnd.Next(nicholeList.Count);
+            ViewBag.RandNicholeName = randNichole;
+            return View("Index");
+        }
+    }
+}
